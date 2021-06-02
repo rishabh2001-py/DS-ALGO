d64ca360afb30652f2e76e3daa70fe4253d9d345
rishabh2001-py <rishabhtripathi.tripathi345@gmail.com>
2021-06-02 16:55:35 +0530

    'Try to revert '
    This reverts commit 9a935df81b1192c9d6389aecd539fc054b3c0f4b.

---
+++ Recursion/Recursion_print_till_number.py
@@ -0,0 +1,47 @@
+
+def dece(n):
+
+    if n == 0:
+        return
+    print(n,end=" ")
+    dece(n-1)
+
+def inc(n):
+    if n == 0:
+        return
+    inc(n-1)
+    print(n,end=" ")
+
+
+
+
+
+
+
+
+
+def main():
+     n=int(input("Enter N:"))
+
+     dece(n)
+     print()
+     inc(n)
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
